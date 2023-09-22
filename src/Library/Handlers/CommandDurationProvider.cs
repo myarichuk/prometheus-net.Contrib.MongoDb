3c9339c33d9c0d6ee5f440ad515693405272f43e
Michael Yarichuk <michael.yarichuk@gmail.com>
2023-09-23 00:52:46 +0300

    chore: add xml comments

--- src/Library/Handlers/CommandDurationProvider.cs
+++ src/Library/Handlers/CommandDurationProvider.cs
@@ -1,32 +1,62 @@
 ﻿using Prometheus;
 using PrometheusNet.MongoDb.Events;
 
-namespace PrometheusNet.MongoDb.Handlers;
-
-public class CommandDurationProvider: IMetricProvider
+namespace PrometheusNet.MongoDb.Handlers
 {
     /// <summary>
-    /// Histogram metric for MongoDB command durations.
+    /// Provides functionality for tracking and recording MongoDB command durations.
     /// </summary>
-    public readonly Histogram CommandDuration = Metrics.CreateHistogram(
-        "mongodb_client_command_duration",
-        "Duration of MongoDB commands (seconds)",
-        new HistogramConfiguration
-        {
-            LabelNames = new[] { "command_type", "status", "target_collection", "target_db" },
-        });
-
-    public void Handle(MongoCommandEventStart e)
+    public class CommandDurationProvider : IMetricProvider
     {
+        private const string SuccessStatus = "success";
+        private const string FailureStatus = "failure";
+
+        /// <summary>
+        /// A histogram metric that captures the duration of MongoDB commands.
+        /// </summary>
+        /// <remarks>
+        /// The metric includes labels for the type of command, the status (success or failure), target collection, and target database.
+        /// Durations are measured in seconds.
+        /// </remarks>
+        public readonly Histogram CommandDuration = Metrics.CreateHistogram(
+            "mongodb_client_command_duration",
+            "Duration of MongoDB commands (seconds)",
+            new HistogramConfiguration
+            {
+                LabelNames = new[] { "command_type", "status", "target_collection", "target_db" },
+            });
+
+        /// <summary>
+        /// Handles the event triggered when a MongoDB command starts.
+        /// </summary>
+        /// <param name="e">Event information for the starting MongoDB command.</param>
+        public void Handle(MongoCommandEventStart e)
+        {
+            // Intentionally left blank.
+        }
+
+        /// <summary>
+        /// Handles the event triggered when a MongoDB command successfully completes.
+        /// </summary>
+        /// <param name="e">Event information for the successfully completed MongoDB command.</param>
+        /// <remarks>
+        /// This will record the duration of successful MongoDB commands in the histogram with appropriate labels.
+        /// </remarks>
+        public void Handle(MongoCommandEventSuccess e) =>
+            CommandDuration
+                .WithLabels(e.OperationRawType, SuccessStatus, e.TargetCollection, e.TargetDatabase)
+                .Observe(e.Duration.GetValueOrDefault().TotalSeconds);
+
+        /// <summary>
+        /// Handles the event triggered when a MongoDB command fails.
+        /// </summary>
+        /// <param name="e">Event information for the failed MongoDB command.</param>
+        /// <remarks>
+        /// This will record the duration of failed MongoDB commands in the histogram with appropriate labels.
+        /// </remarks>
+        public void Handle(MongoCommandEventFailure e) =>
+            CommandDuration
+                .WithLabels(e.OperationRawType, FailureStatus, e.TargetCollection, e.TargetDatabase)
+                .Observe(e.Duration.GetValueOrDefault().TotalSeconds);
     }
-
-    public void Handle(MongoCommandEventSuccess e) =>
-        CommandDuration
-            .WithLabels(e.OperationRawType, "success", e.TargetCollection, e.TargetDatabase)
-            .Observe(e.Duration.GetValueOrDefault().TotalSeconds);
-
-    public void Handle(MongoCommandEventFailure e) =>
-        CommandDuration
-            .WithLabels(e.OperationRawType, "failure", e.TargetCollection, e.TargetDatabase)
-            .Observe(e.Duration.GetValueOrDefault().TotalSeconds);
 }
