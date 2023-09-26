fb6b61d92f300fae250c090d67b3dfe29a662b18
Michael Yarichuk <michaely@bindecy.com>
2023-09-26 20:38:19 +0300

    feat: add mongodb_client_command_duration_summary and adjust buckets of the command duration

--- src/Library/Handlers/CommandDurationMetricProvider.cs
+++ src/Library/Handlers/CommandDurationMetricProvider.cs
@@ -18,13 +18,30 @@ internal class CommandDurationMetricProvider : IMetricProvider
     /// The metric includes labels for the type of command, the status (success or failure), target collection, and target database.
     /// Durations are measured in seconds.
     /// </remarks>
-    public readonly Histogram CommandDuration = Metrics.CreateHistogram(
+    public readonly Histogram CommandDurationHistogram = Metrics.CreateHistogram(
         "mongodb_client_command_duration",
         "Duration of MongoDB commands (seconds)",
         new HistogramConfiguration
         {
+            Buckets = new[] { 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 40, 60 },
             LabelNames = new[] { "command_type", "status", "target_collection", "target_db" },
         });
+
+    /// <summary>
+    /// A Summary metric that captures the duration of MongoDB commands.
+    /// </summary>
+    /// <remarks>
+    /// The metric includes labels for the type of command, the status (success or failure), target collection, and target database.
+    /// Durations are measured in seconds.
+    /// </remarks>
+    public readonly Summary CommandDurationSummary = Metrics.CreateSummary(
+        "mongodb_client_command_duration_summary",
+        "Duration of MongoDB commands (seconds)",
+        new SummaryConfiguration
+        {
+            LabelNames = new[] { "command_type", "status", "target_collection", "target_db" },
+        });
+
 
     /// <summary>
     /// Handles the event triggered when a MongoDB command successfully completes.
@@ -33,10 +50,16 @@ internal class CommandDurationMetricProvider : IMetricProvider
     /// <remarks>
     /// This will record the duration of successful MongoDB commands in the histogram with appropriate labels.
     /// </remarks>
-    public void Handle(MongoCommandEventSuccess e) =>
-        CommandDuration
+    public void Handle(MongoCommandEventSuccess e)
+    {
+        CommandDurationHistogram
             .WithLabels(e.OperationRawType, SuccessStatus, e.TargetCollection, e.TargetDatabase)
             .Observe(e.Duration.GetValueOrDefault().TotalSeconds);
+
+        CommandDurationSummary
+            .WithLabels(e.OperationRawType, SuccessStatus, e.TargetCollection, e.TargetDatabase)
+            .Observe(e.Duration.GetValueOrDefault().TotalSeconds);
+    }
 
     /// <summary>
     /// Handles the event triggered when a MongoDB command fails.
@@ -46,7 +69,7 @@ internal class CommandDurationMetricProvider : IMetricProvider
     /// This will record the duration of failed MongoDB commands in the histogram with appropriate labels.
     /// </remarks>
     public void Handle(MongoCommandEventFailure e) =>
-        CommandDuration
+        CommandDurationHistogram
             .WithLabels(e.OperationRawType, FailureStatus, e.TargetCollection, e.TargetDatabase)
             .Observe(e.Duration.GetValueOrDefault().TotalSeconds);
 }
