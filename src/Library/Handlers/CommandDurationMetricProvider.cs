bf49938bfdb2ec829cf616796f8ff23f6a4079d7
Michael Yarichuk <michael.yarichuk@gmail.com>
2023-09-28 10:51:57 +0300

    refactor!: remove redundant metric "mongodb_client_command_duration_summary" - it provides roughly the same metrics

--- src/Library/Handlers/CommandDurationMetricProvider.cs
+++ src/Library/Handlers/CommandDurationMetricProvider.cs
@@ -28,42 +28,16 @@ internal class CommandDurationMetricProvider : IMetricProvider
         });
 
     /// <summary>
-    /// A Summary metric that captures the duration of MongoDB commands.
-    /// </summary>
-    /// <remarks>
-    /// The metric includes labels for the type of command, the status (success or failure), target collection, and target database.
-    /// Durations are measured in seconds.
-    /// </remarks>
-    public readonly Summary CommandDurationSummary = Metrics.CreateSummary(
-        "mongodb_client_command_duration_summary",
-        "Duration of MongoDB commands (seconds)",
-        new SummaryConfiguration
-        {
-            LabelNames = new[] { "command_type", "status", "target_collection", "target_db" },
-        });
-
-    public void Handle(MongoCommandEventStart e)
-    {
-        
-    }
-
-    /// <summary>
     /// Handles the event triggered when a MongoDB command successfully completes.
     /// </summary>
     /// <param name="e">Event information for the successfully completed MongoDB command.</param>
     /// <remarks>
     /// This will record the duration of successful MongoDB commands in the histogram with appropriate labels.
     /// </remarks>
-    public void Handle(MongoCommandEventSuccess e)
-    {
+    public void Handle(MongoCommandEventSuccess e) =>
         CommandDurationHistogram
             .WithLabels(e.OperationRawType, SuccessStatus, e.TargetCollection, e.TargetDatabase)
             .Observe(e.Duration.GetValueOrDefault().TotalSeconds);
-
-        CommandDurationSummary
-            .WithLabels(e.OperationRawType, SuccessStatus, e.TargetCollection, e.TargetDatabase)
-            .Observe(e.Duration.GetValueOrDefault().TotalSeconds);
-    }
 
     /// <summary>
     /// Handles the event triggered when a MongoDB command fails.
